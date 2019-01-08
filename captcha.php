4a7df3c9b9d9e73043595cd263a75b9995e37851
Kento <rogeraabbccdd@users.noreply.github.com>
2019-01-08 10:43:19 +0800

    Add files via upload

---
+++ captcha.php
@@ -0,0 +1,15 @@
+<?php
+	for($i=0;$i<4;$i++){
+		$num[] = chr(rand(65,90));
+	}
+?>
+<xml version="1.0" encoding="utf-8">
+<!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="圖層_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="500px" height="400px" viewBox="0 0 500 400" enable-background="new 0 0 500 400" xml:space="preserve">
+<text transform="matrix(1 0 0 1 <?=rand(0,105)?> <?=rand(100,300)?>)" font-family="'AdobeMingStd-Light-B5pc-H'" font-size="72"><?=$num[0]?></text>
+<text transform="matrix(1 0 0 1 <?=rand(126,230)?> <?=rand(100,300)?>)" font-family="'AdobeMingStd-Light-B5pc-H'" font-size="72"><?=$num[1]?></text>
+<text transform="matrix(1 0 0 1 <?=rand(251,335)?> <?=rand(100,300)?>)" font-family="'AdobeMingStd-Light-B5pc-H'" font-size="72"><?=$num[2]?></text>
+<text transform="matrix(1 0 0 1 <?=rand(376,400)?> <?=rand(100,300)?>)" font-family="'AdobeMingStd-Light-B5pc-H'" font-size="72"><?=$num[3]?></text>
+</svg>
